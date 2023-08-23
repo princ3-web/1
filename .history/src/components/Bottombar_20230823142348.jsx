3939149d3656b9215cb4fffb508db5b0044a4ffd
Michal Gornicki <michal.gornicki@ithouse.co>
2023-08-23 14:45:01 +0200

    first commit

---
+++ .history/src/components/Bottombar_20230823142348.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { blueGrey } from "@mui/material/colors";
+
+const Bottombar = () => {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "153px",
+        
+        backgroundColor: blueGrey[900],
+      }}
+    ></Box>
+  );
+};
+
+export default Bottombar;
---
+++ .history/src/components/Bottombar_20230823144120.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { grey, blueGrey, blue, teal } from "@mui/material/colors";
+import github from "../assets/icons/github1.png";
+import tiktok from "../assets/icons/tiktok.png";
+import linkedin from "../assets/icons/linkedin.png";
+import discord from "../assets/icons/discord.png";
+import {CardMedia} from "@mui/material";
+
+const Bottombar = () => {
+  const icons = [github, tiktok, linkedin, discord];
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        width: "100%",
+        height: "126px",
+        backgroundColor: blueGrey[900],
+        boxSizing: "border-box",
+        pr: "180px",
+      }}
+    >
+      <Box sx={{ display: "flex" }}>
+        {icons.map((item) => (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              ml: 2,
+              borderRadius: 2,
+              cursor: "pointer",
+              "&:hover": {
+                backgroundColor: blueGrey[800],
+              },
+            }}
+          >
+            <CardMedia
+              component="img"
+              image={item}
+              alt="Sample Image"
+              sx={{ height: "20px", width: "20px", objectFit: "contain" }}
+            />
+          </Box>
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default Bottombar;
