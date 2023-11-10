2e5c40655f970bd44c5a7f3ee845a629d22a6592
Michal Gornicki <michal.gornicki@ithouse.co>
2023-11-10 14:03:53 +0100

    1345

--- .history/src/components/Bottombar_20231110140257.jsx
+++ .history/src/components/Bottombar_20231110140257.jsx
@@ -2,28 +2,26 @@ import React from "react";
 import { Box, Button, Link, Typography } from "@mui/material";
 import { grey, blueGrey, blue, teal } from "@mui/material/colors";
 
+
 const Bottombar = ({ isMobile }) => {
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        alignItems: isMobile ? "flex-start" : "flex-end",
+        alignItems: "flex-end",
         justifyContent: "center",
         width: "100vw",
         height: "126px",
         backgroundColor: blueGrey[900],
         boxSizing: "border-box",
         pr: isMobile ? "unset" : "180px",
-        pl: isMobile ? "40px" : "unset",
       }}
     >
       <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-        <Typography sx={{ color: grey[400], fontSize: 14, fontWeight: 900 }}>
-          tel: 662 047 277
-        </Typography>
+        <Typography sx={{ color: grey[400], fontSize:14, fontWeight: 900 }}>tel: 662 047 277</Typography>
         <Link href={"mailto:gornmich@gmail.com"} sx={{ textDecoration: "none", color: grey[400] }}>
-          <Typography sx={{ fontSize: 14, fontWeight: 900 }}>e-mail: gornmich@gmail.com</Typography>
+          <Typography sx={{fontSize:14, fontWeight: 900 }}>e-mail: gornmich@gmail.com</Typography>
         </Link>
       </Box>
     </Box>
